4f5b91e37a1427b0e149f2b1ded51ab7d8e02a67
amir <amir.rashidy78@gmail.com>
2024-05-10 15:58:48 +0330

    intialize repo

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+
+<template>
+  <div>
+    <header>
+     <h1>map</h1>
+    </header>
+  
+    <main>
+      <Map></Map>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import Map from './components/map.vue'
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+main {
+  height: 30rem;
+  width: 100%;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/map.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page">
+      <NeshanMap
+          defaultType="neshan"
+          mapKey="web.f656d456a7ef48f0af42912ba05da8a6"
+          serviceKey="service.0a789d697fce4b0697252fe64cb4e4b9"
+          :center="{latitude: 36.311559, longitude: 59.5870851}"
+          :zoom="14"
+          :poi="true"
+          :traffic="true"
+          @on-init="onInit"
+      />
+  </div>
+</template>
+<script setup lang="ts">
+import NeshanMap from "@neshan-maps-platform/vue3-openlayers"
+import { Map } from "@neshan-maps-platform/ol"
+import {fromLonLat} from '@neshan-maps-platform/ol/proj'
+
+function onInit(map:Map) {
+    const view = map?.getView()
+    view?.animate({
+        center: olProj.fromLonLat([59.5870851, 36.311559]),
+        zoom: 11,
+        duration: 1000,
+    })
+}
+</script>
+<style>
+  @import url("@neshan-maps-platform/vue3-openlayers/dist/style.css");
+  html, body, .page, #app {
+      width: 100%;
+      height: 100%;
+      display: block;
+      padding: 0;
+      margin: 0;
+  }
+</style>
