6456baf5eee31e59aa096f04f04163e152aad316
amir <amir.rashidy78@gmail.com>
2024-05-10 17:10:55 +0330

    add address finder

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,24 @@
     <main>
       <Map></Map>
     </main>
+    <button @click="address">
+      click
+    </button>
   </div>
 </template>
 
 <script setup>
 import Map from './components/map.vue'
+
+async function address (){
+  const data = await fetch('https://api.neshan.org/v5/reverse?lat=15&lng=12' , {
+    method: 'GET',
+    headers: {
+      'Api-Key': 'service.0a789d697fce4b0697252fe64cb4e4b9'
+    }
+  })
+  console.log(data)
+}
 </script>
 
 <style scoped>
