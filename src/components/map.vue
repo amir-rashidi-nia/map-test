1325cc99c524520338a4361d2249bd416f3fe005
amir <amir.rashidy78@gmail.com>
2024-05-11 20:48:53 +0330

    add service key

--- src/components/map.vue
+++ src/components/map.vue
@@ -3,8 +3,7 @@
       <NeshanMap
           defaultType="neshan"
           mapKey="web.3d0e1c00a26e4727ab55d483d0bd7c85"
-          serviceKey=""
-          :center="{latitude: 36.311559, longitude: 59.5870851}"
+          serviceKey="service.0a789d697fce4b0697252fe64cb4e4b9"
           :zoom="14"
           :poi="true"
           :traffic="true"
